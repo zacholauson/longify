e89112ceea1fc4cc23bc0dbc6e0b534f35c34a7c
Zach Olauson <zmolauson@gmail.com>
2013-10-31 18:40:27 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Longify::Application.config.secret_key_base = 'b018d2af582a514f49c84e87ef5c22ce0d16fb05eab1ac91402e6309fb2d13a667bd587c0b61739e4b8f92a3b0881ff845690179af74e7f9a4ad64bbb4b55f4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Longify::Application.config.session_store :cookie_store, key: '_Longify_session'
