75e2055d01079dbd85e7426fd6351fcb383f5405
Zach Olauson <zmolauson@gmail.com>
2013-11-02 14:03:02 -0500

    setup longify controller and model

---
+++ app/controllers/longify_controller.rb
@@ -0,0 +1,34 @@
+class LongifyController < ApplicationController
+  def index
+  end
+
+  def new
+    @url = Url.new
+  end
+
+  def create
+    begin
+      url = Url.find_or_create_by!(original_url: params[:original_url])
+    rescue
+      flash[:notice] = "Invalid Url"
+      redirect_to new_longified_path
+    end
+    redirect_to show_longified_path(url)
+  end
+
+  def show
+    @url = Url.friendly.find(params[:id])
+  end
+
+  def redirect
+    url = Url.find_by_longified_url(params[:longified_url])
+
+    if url
+      url.visits += 1
+      url.save
+      redirect_to(url.original_url)
+    else
+      redirect_to(:index)
+    end
+  end
+end
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,15 @@ class Url < ActiveRecord::Base
   extend FriendlyId
   friendly_id :original_url, use: :slugged
 
-  validates_presence_of :original_url, :longified_url, :visits
+  validates_presence_of :original_url, :visits
   validates_uniqueness_of :original_url, :longified_url
+  validates_format_of :original_url, :with => URI::regexp(%w(http https))
+
+  before_create :generate_longified_url
+
+  private
+
+  def generate_longified_url
+    self.longified_url = (0...1000).map{ ('a'..'z').to_a[rand(26)] }.join
+  end
 end
