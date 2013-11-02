2e71bcefd2af442c96bb67ffc18d803674d13b99
Zach Olauson <zmolauson@gmail.com>
2013-11-02 17:34:20 -0500

    fixed routing issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Longify::Application.routes.draw do
-  get  '/show/:id', to: 'longify#show', as: :show_longified
-  post '/create', to: 'longify#create', as: :create_longified
-  get  '/:longified_url', to: 'longify#redirect', as: :redirect_to_original
+  resources :urls
+  get '/:longified_url', to: 'urls#redirect'
 
-  root to: 'longify#new'
+  root 'urls#new'
 end
